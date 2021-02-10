05b95ce520e3e0d6870ea7ad5c7fc8d4c8790058
Pierre Bretéché <pbreteche@dawan.fr>
2021-02-10 11:43:50 +0100

    first static routes

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,14 +1,20 @@
+import { ContactNewFormComponent } from './../contact-new-form/contact-new-form.component';
+import { ContactNavComponent } from './../contact-nav/contact-nav.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  { path: '', component: ContactNavComponent },
+  { path: 'nouveau', component: ContactNewFormComponent },
 ];
 
 @NgModule({
   declarations: [],
   imports: [
     RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class RoutingModule { }
