56e40a6f311c77177bd9b272e3e107cdfe9b23bf
Pierre Bretéché <pbreteche@dawan.fr>
2021-02-10 13:58:08 +0100

    observe route param

--- src/app/contact-detail/contact-detail.component.ts
+++ src/app/contact-detail/contact-detail.component.ts
@@ -1,13 +1,22 @@
 import { CONTACTS } from './../../data/contacts';
 import { Contact } from './../../model/contact';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-contact-detail',
   templateUrl: './contact-detail.component.html',
   styleUrls: ['./contact-detail.component.scss']
 })
-export class ContactDetailComponent {
-  contact: Contact = CONTACTS[0]; // temporary, we will get real contact from path
+export class ContactDetailComponent implements OnInit {
+  contact: Contact = CONTACTS[0];
 
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(map => {
+      const id = +(map.get('id') || 1);
+      this.contact = CONTACTS[id - 1];
+    });
+  }
 }
