63ee078d2c06c9023976ab378a69cb7318a9970a
Pierre Bretéché <pbreteche@dawan.fr>
2021-02-12 13:41:03 +0100

    services config demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { UserService } from './user.service';
 import { RoutingModule } from './routing/routing.module';
-import { NgModule } from '@angular/core';
+import { NgModule, InjectionToken } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, NG_VALIDATORS } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -16,6 +17,14 @@ import { AlphanumericValidatorDirective } from './alphanumeric-validator.directi
 import { MinValidatorDirective } from './min-validator.directive';
 import { EmailPartPipe } from './email-part.pipe';
 import { LoginComponent } from './login/login.component';
+
+const API_URL = new InjectionToken('app.api-url');
+const API_URL_VALUE = 'http://localhost:4200';
+const userServiceFactory = function(defaultUsername) {
+  const service = new UserService();
+  service.user = defaultUsername;
+  return service;
+}
 
 @NgModule({
   declarations: [
@@ -39,7 +48,18 @@ import { LoginComponent } from './login/login.component';
     RoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    UserService, // class, short syntax
+    { provide: UserService, useClass: UserService }, // class, explicit syntax
+    // création d'un alias (useExisting)
+    // ajout dans un conteneur multiple de service (multi: true)
+    // => ajoute une référence vers un service existant, dans le conteneur
+    { provide: NG_VALIDATORS, useExisting: MinValidatorDirective, multi: true },
+    // use non-object value
+    // => need to manually create DI token
+    { provide: API_URL, useValue: API_URL_VALUE },
+    { provide: UserService, useFactory: userServiceFactory, deps: ['John Doe'] }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
