84f28427a886ed739c64d773eada115e23f92052
Pierre Bretéché <pbreteche@dawan.fr>
2021-02-12 11:52:47 +0100

    begin of alert message service

--- src/app/authenticated.guard.ts
+++ src/app/authenticated.guard.ts
@@ -1,18 +1,28 @@
 import { UserService } from './user.service';
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AlertMessageService } from './alert-message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticatedGuard implements CanActivate {
-  constructor(private user: UserService) {}
+  constructor(
+    private user: UserService,
+    private alert: AlertMessageService
+  ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
 
-    return !!this.user.user;
+    const result = !!this.user.user;
+    
+    if (!result) {
+      this.alert.messages.push('Vous devez vous connecter pour accéder à cette page');
+    }
+
+    return result;
   }
   
 }
